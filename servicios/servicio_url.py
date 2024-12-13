508b3c9be31a006d63a07578d288c3c855a886fe
Dnaet <54982189+Dnaet@users.noreply.github.com>
2024-12-13 19:45:41 -0300

    13/12

--- servicios/servicio_url.py
+++ servicios/servicio_url.py
@@ -1,8 +1,8 @@
-import auxiliares.constante as url_base_jsonplaceholder
+import auxiliares.constante as URL_BASE
 
 
 def url_servicio(origen):
     #direccion = f"{url_base}{origen}"
-    direccion = f"{url_base_jsonplaceholder}{origen}"
+    direccion = f"{URL_BASE}{origen}"
     #direccion = f"https://jsonplaceholder.typicode.com/{origen}"
     return direccion
